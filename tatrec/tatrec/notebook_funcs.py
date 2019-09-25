613d9bb52595145b0ff420579fe97cab6b4d063a
Jeff Spencer <jeffspencerd@gmail.com>
2019-09-25 18:14:41 -0400

    Creating lsh model for quick search of similar tattoos

--- tatrec/tatrec/notebook_funcs.py
+++ tatrec/tatrec/notebook_funcs.py
@@ -1,5 +1,7 @@
 from fastai.vision import ImageList, imagenet_stats, get_transforms
-
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
 # Notebook helper functions
 
 
@@ -26,3 +28,42 @@ def get_data_from_folder(path, bs, img_size, tfms=get_transforms(), extensions=[
             .transform(tfms, size=img_size)
             .databunch(bs=bs, num_workers=0).normalize(imagenet_stats))
     return data
+
+
+def print_data_classes_size(data):
+    """Print number and name of data classes. Also, size of training and validation sets.
+    """
+    print('Number of classes {0}'.format(data.c))
+    print(data.classes)
+    print('Train dataset size: {0}'.format(len(data.train_ds.x)))
+    print('Test dataset size: {0}'.format(len(data.valid_ds.x)))
+
+
+def plot_similar_tats(idx, feature_dict, lsh_variable, n_items=6):
+    """Takes an input index for the training set and plots the closest matching tattoos to that input
+    tattoo.
+
+    Args
+    ----------
+    idx : index to tattoo in the training set
+    feature_dict : wraps both image locations and feature vectors at the output of the cnn before
+    the final layer.
+    lsh_variable : trained lsh model to query the input image
+    n_items : number of items to return
+
+    Returns
+    -------
+    plt.show() : matplotlib grid plot of the index image first and n other similar images
+
+    """
+    response = lsh_variable.query(feature_dict[list(feature_dict.keys())[idx]].flatten(),
+                                  num_results=n_items + 1, distance_func='hamming')
+    columns = 3
+    rows = int(np.ceil(n_items+1 / columns))
+    fig = plt.figure(figsize=(2 * rows, 5 * rows))
+    for i in range(1, columns*rows + 1):
+        if i < n_items + 2:
+            img = Image.open(response[i-1][0][1])
+            fig.add_subplot(rows, columns, i)
+            plt.imshow(img)
+    return plt.show()
