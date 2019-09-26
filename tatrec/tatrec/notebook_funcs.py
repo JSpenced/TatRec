758b8479a0cec6b58156d89de4fe850331b56cb6
Jeff Spencer <jeffspencerd@gmail.com>
2019-09-26 10:50:22 -0400

    Add a plot tats from a lsh query function

--- tatrec/tatrec/notebook_funcs.py
+++ tatrec/tatrec/notebook_funcs.py
@@ -68,12 +68,32 @@ def plot_similar_tats_idx(idx, feature_dict, lsh_variable, n_items=6, distance_f
     """
     response = lsh_variable.query(feature_dict[list(feature_dict.keys())[idx]].flatten(),
                                   num_results=n_items + 1, distance_func=distance_func)
+
+    return plot_similar_tats_query(response, n_items=n_items + 1, distance_func=distance_func)
+
+
+def plot_similar_tats_query(lsh_response, n_items=6, distance_func='hamming'):
+    """Takes a lsh query and plots the closest matching tattoos to that input tattoo.
+
+    Args
+    ----------
+    lsh_response : lsh response variable containing a dictionary with values and image locations
+    n_items : number of items to return
+    distance_func : The distance function to be used. Currently it needs to be one of ("hamming",
+    "euclidean", "true_euclidean", "centred_euclidean", "cosine", "l1norm"). By default "hamming"
+    will used.
+
+    Returns
+    -------
+    plt.show() : matplotlib grid plot of the index image first and n other similar images
+
+    """
     columns = 3
     rows = int(np.ceil(n_items+1 / columns))
     fig = plt.figure(figsize=(2 * rows, 5 * rows))
     for i in range(1, columns*rows + 1):
-        if i < n_items + 2:
-            img = Image.open(response[i-1][0][1])
+        if i < n_items + 1:
+            img = Image.open(lsh_response[i-1][0][1])
             fig.add_subplot(rows, columns, i)
             plt.imshow(img)
     return plt.show()
