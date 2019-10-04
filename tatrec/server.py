e7f56be3900bc8125dab548c2ebc08a25a8ca922
Jeff Spencer <jeffspencerd@gmail.com>
2019-10-03 23:48:54 -0400

    Add drop-down to select images for demo

--- tatrec/server.py
+++ tatrec/server.py
@@ -3,6 +3,7 @@ from flask import render_template, request, flash, session
 from werkzeug.utils import secure_filename
 from app import app
 import os
+import shutil
 import json
 import lzma
 from sys import platform
@@ -29,7 +30,7 @@ def get_index_page():
                            fols_rec4=session['fols_rec4'], user_rec5=session['user_rec5'],
                            fols_rec5=session['fols_rec5'], likes_rec1=session['likes_rec1'],
                            likes_rec2=session['likes_rec2'], likes_rec3=session['likes_rec3'],
-                           likes_rec4=session['likes_rec4'], likes_rec5=session['likes_rec5'] )
+                           likes_rec4=session['likes_rec4'], likes_rec5=session['likes_rec5'])
 
 
 def load_user_from_json(path):
@@ -48,45 +49,58 @@ def set_session_recs(rec_num, username, followers, likes, img_path):
     session['img_rec' + str(rec_num+1)] = img_path
 
 
+def delete_files_from_folder(path):
+    for the_file in os.listdir(path):
+        file_path = os.path.join(path, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(e)
+
+
 @app.route('/recommendations', methods=['GET', 'POST'])
 def get_recs():
     """Return rendered web page with the recommended tattoo artists when a user clicks
     `Get tattoo Recs`
     """
     if request.method == 'POST':
+        tat_dropdown = request.form.get('tatlist')
         # check if the post request has the file part
-        if 'file' not in request.files:
-            flash("No file part, file didn't get sent to server. Please retry.")
-            return get_index_page()
-        img_file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if img_file.filename == '':
-            flash('No selected file, please select a file to get your recs!')
-            return get_index_page()
-        if img_file and allowed_file(img_file.filename):
-            filename = secure_filename(img_file.filename)
-            img_folder_path = app.config['UPLOAD_FOLDER']
-            img_full_path = os.path.join(img_folder_path, filename)
-            for the_file in os.listdir(img_folder_path):
-                file_path = os.path.join(img_folder_path, the_file)
-                try:
-                    if os.path.isfile(file_path):
-                        os.unlink(file_path)
-                except Exception as e:
-                    print(e)
+        img_folder_path = app.config['UPLOAD_FOLDER']
+        if tat_dropdown == '':
+            if 'file' not in request.files:
+                flash("No file part, file didn't get sent to server. Please retry.")
+                return get_index_page()
+            img_file = request.files['file']
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if img_file.filename == '':
+                flash('No selected file, please select a file to get your recs!')
+                return get_index_page()
+            if img_file and allowed_file(img_file.filename):
+                filename = secure_filename(img_file.filename)
+                img_full_path = os.path.join(img_folder_path, filename)
+                delete_files_from_folder(img_folder_path)
             if img_full_path[-5:] == ".jpeg":
                 img_full_path = img_full_path[:-5] + ".jpg"
             img_file.save(os.path.join(img_full_path))
-            img_paths = tat_recognizer.get_tattoo_recs()
-            session['img_full_path'] = img_full_path
-            for i, path in enumerate(img_paths):
-                if path[-3:] == 'UTC':
-                    username, followers, likes = load_user_from_json(path)
-                else:
-                    username, followers, likes = load_user_from_json(path[:-2])
-                set_session_recs(i, username, followers, likes, img_paths[i] + ".jpg")
-            return get_index_page()
+        else:
+            delete_files_from_folder(img_folder_path)
+            img_name = 'tatrec--demo--' + tat_dropdown + '.jpg'
+            print(os.path.join(img_folder_path, img_name))
+            img_full_path = shutil.copy("./static/img/demo/" + img_name,
+                                        os.path.join(img_folder_path, img_name))
+            print(img_full_path)
+        img_paths = tat_recognizer.get_tattoo_recs()
+        session['img_full_path'] = img_full_path
+        for i, path in enumerate(img_paths):
+            if path[-3:] == 'UTC':
+                username, followers, likes = load_user_from_json(path)
+            else:
+                username, followers, likes = load_user_from_json(path[:-2])
+            set_session_recs(i, username, followers, likes, img_paths[i] + ".jpg")
+        return get_index_page()
     return get_index_page()
 
 
